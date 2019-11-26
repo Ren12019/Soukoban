ce72d2e0b0283c9cf49f0d16bdfc74c44fa522f5
Ren <gshock1201@gmail.com>
2019-11-26 13:20:06 +0900

    searchBoxの追加
    荷物が存在する位置を求める関数を追加

--- Soukoban/search.cpp
+++ Soukoban/search.cpp
@@ -6,16 +6,32 @@
 using namespace std; 
 using TargetStage = vector<vector<SQUARE>>;
 
+SQUARE searchBox(int stage[][WIDTH]) {
+	SQUARE point_box;
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			if (stage[y][x] == BOX) {
+				point_box.x = x;
+				point_box.y = y;
+				return point_box;
+			}
+		}
+	}
+}
 
 int searchBreadthFirst(int stage[][WIDTH]) {
 	int search_stage[HEIGHT][WIDTH] = {};
+	TargetStage Target;
+	SQUARE point_box;
 
 	//init
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++) {
-			search_stage[y][x] = stage[y][x];
+			search_stage[y][x] == stage[y][x];
 		}
 	}
+	point_box = searchBox(search_stage);
+	 //create Graph
 
 	return 0;
 }
